f76c0b8d831162769f9bf547e788226f7e6ffead
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2026-07-27 23:32:32 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::process::ExitCode;
-
 mod cli;
 mod credentials;
 mod datadog;
@@ -7,12 +5,6 @@ mod fastly;
 mod guest;
 mod snapshot;
 
-fn main() -> ExitCode {
-    match cli::run() {
-        Ok(()) => ExitCode::SUCCESS,
-        Err(error) => {
-            eprintln!("{error:#}");
-            ExitCode::FAILURE
-        }
-    }
+fn main() -> anyhow::Result<()> {
+    cli::run()
 }
